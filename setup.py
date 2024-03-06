67eda9e285e6612c4e7fb75e8efc050744f62278
jmaxmarno <jmaxmarno@gmail.com>
2024-03-06 12:21:07 -0700

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name='wmo-uasdc',
+setup(name='uas-utils',
       version='1.0.0',
       description='Utilities for WMO UASDC project',
       author='James Simkins',
