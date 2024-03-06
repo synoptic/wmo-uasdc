eb53cb872ae7026c79bd8793427e81d6bf5d0276
jmaxmarno <jmaxmarno@gmail.com>
2024-03-06 11:59:12 -0700

    name vs package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup, find_packages
 
-setup(name='UAS Utils',
+setup(name='wmo-uasdc',
       version='1.0.0',
       description='Utilities for WMO UASDC project',
       author='James Simkins',
       author_email='james.simkins@synopticdata.com',
       url='https://github.com/synoptic/wmo-uasdc',
-      packages=find_packages()
+      packages=['uas-utils'],
       )
