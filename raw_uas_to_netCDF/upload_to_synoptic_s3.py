57616baa2d70d80ccbef876ff309019e3246a087
jmaxmarno <jmaxmarno@gmail.com>
2024-03-07 17:07:49 -0700

    tweak upload docs

--- raw_uas_to_netCDF/upload_to_synoptic_s3.py
+++ raw_uas_to_netCDF/upload_to_synoptic_s3.py
@@ -4,18 +4,20 @@ import boto3
 aws_access_key_id = ""
 # secret access key sent separately
 aws_secret_access_key = ""
+# bucket name
+bucket_name = "provided-bucket-name"
+# path to file
+local_filepath = "../nc2bufr/UASDC_operatorID_airframeID_20230327025804Z.nc"
+# appropriate prefix syntax for storage in s3 entry bucket
+s3_filepath = "operatorID/airframeID/processingLevel/YYYY/MM/operatorID_airframeID_20230327025804Z.nc"
 
-bucket_name = "uas_utils-synoptic-bucket"
-
-local_filepath = "UASDC_operatorID_airframeID_processingLevel_20230327030016Z.nc"  
-s3_filepath = "operatorID/airframeID/processingLevel/YYYY/MM/operatorID_airframeID_processingLevel_YYYYMMDDHHMMSSZ.nc"
 
 # Create an S3 client
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 # Upload the file to the S3 bucket
 try:
-    s3.upload_file(local_filepath, bucket_name, s3_filename)
+    s3.upload_file(local_filepath, bucket_name, s3_filepath)
     print(f"File {local_filepath} uploaded to {bucket_name}/{s3_filepath}")
 except Exception as e:
     print(f"An error occurred: {e}")
